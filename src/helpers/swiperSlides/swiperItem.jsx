333e81858350cd0a9226376e162c98611d025572
KhailoAndrey <hap512@gmail.com>
2024-01-19 14:45:36 +0200

    test

--- src/helpers/swiperSlides/swiperItem.jsx
+++ src/helpers/swiperSlides/swiperItem.jsx
@@ -4,15 +4,27 @@ import { useState, useEffect } from 'react';
 const SwiperSlideImg = ({ photo, alt }) => {
   const [slide, setSlide] = useState(null);
 
+  // useEffect(() => {
+  //   import.meta.env
+  //     .import(photo)
+  //     .then(image => {
+  //       setSlide(image.default);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error loading the image:', error);
+  //     });
+  // }, [photo]);
   useEffect(() => {
-    import.meta.env
-      .import(photo)
-      .then(image => {
-        setSlide(image.default);
-      })
-      .catch(error => {
+    const importImage = async () => {
+      try {
+        const imageModule = await import(`../../../public/${photo}`);
+        setSlide(imageModule.default);
+      } catch (error) {
         console.error('Error loading the image:', error);
-      });
+      }
+    };
+
+    importImage();
   }, [photo]);
   return <>{slide && <img src={slide} alt={alt} />}</>;
 };
