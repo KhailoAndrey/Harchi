199ab6d22311f8ac3e1255a79a340d6a5083584d
KhailoAndrey <hap512@gmail.com>
2024-01-19 14:56:24 +0200

    return code before refactoring

--- src/helpers/swiperSlides/swiperItem.jsx
+++ src/helpers/swiperSlides/swiperItem.jsx
@@ -4,27 +4,14 @@ import { useState, useEffect } from 'react';
 const SwiperSlideImg = ({ photo, alt }) => {
   const [slide, setSlide] = useState(null);
 
-  // useEffect(() => {
-  //   import.meta.env
-  //     .import(photo)
-  //     .then(image => {
-  //       setSlide(image.default);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error loading the image:', error);
-  //     });
-  // }, [photo]);
   useEffect(() => {
-    const importImage = async () => {
-      try {
-        const imageModule = await import(`../../../public/${photo}`);
-        setSlide(imageModule.default);
-      } catch (error) {
+    import(photo)
+      .then(image => {
+        setSlide(image.default);
+      })
+      .catch(error => {
         console.error('Error loading the image:', error);
-      }
-    };
-
-    importImage();
+      });
   }, [photo]);
   return <>{slide && <img src={slide} alt={alt} />}</>;
 };
