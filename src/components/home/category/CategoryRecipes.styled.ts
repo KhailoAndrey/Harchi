9805c0f864ffd728eb1f1e7d73111afb88a3fccb
KhailoAndrey <hap512@gmail.com>
2024-01-26 19:41:01 +0200

    add category swiper

---
+++ src/components/home/category/CategoryRecipes.styled.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 1280px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+  margin-bottom: 100px;
+  /* overflow: hidden; */
+`;
+
+export const Title = styled.span`
+  color: #111110;
+  font-family: 'KyivType Sans';
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 130%;
+`;
+
+export const Slider = styled.div`
+  width: 1280px;
+  height: 464px;
+  padding: 0;
+  display: flex;
+`;
