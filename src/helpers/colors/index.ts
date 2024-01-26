8aec5d3c328bfb1f1e923a7e7d99ef6621d23de7
Marina Sheigets <marinasejgec34@gmail.com>
2024-01-26 16:53:54 +0200

    feat: add palette for colors

---
+++ src/helpers/colors/index.ts
@@ -0,0 +1,25 @@
+export const palette = {
+  mainBlack: '#111110',
+  orange: '#EE7214',
+  mainWhite: '#F8F8F6',
+  green: '#527853',
+  red: '#9C2B3F',
+  darkGreen: '#1E261E',
+
+  darkGrey: '#403F3F',
+  grey: '#6F6B6B',
+  lightGrey: '#CCCBCB',
+  link: '#007CD4',
+  fill: '#EDEDED',
+  stroke: '#D8C9B7',
+};
+
+export const gradient = {
+  buttonOrange:
+    'background: linear-gradient(124.14deg, #EE7214 13.2%, #F4A322 63.1%, #FAD531 93.71%);',
+
+  red2: 'background: linear-gradient(91.21deg, #B6143B 6.48%, #C84738 43.92%, #FAD531 98.97%);  ',
+  red: 'background: linear-gradient(91.21deg, #FCCD00 12.95%, #C84738 70.76%, #B6143B 98.97%); ',
+  whiteBlack:
+    'background: linear-gradient(354.43deg, #242323 15.32%, rgba(68, 65, 65, 0.776042) 56.05%, rgba(99, 95, 95, 0.552083) 70.82%, rgba(142, 136, 136, 0.247486) 89.15%, rgba(176, 169, 169, 0) 100.35%);',
+};
