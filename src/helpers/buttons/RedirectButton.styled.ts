46259fbab42092a006db69d1e843314ff16f3810
KhailoAndrey <hap512@gmail.com>
2024-01-27 14:48:34 +0200

    add animation to hero button

--- src/helpers/buttons/RedirectButton.styled.ts
+++ src/helpers/buttons/RedirectButton.styled.ts
@@ -1,4 +1,13 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  0%, 100% {
+    width: 194px;
+  }
+  50% {
+    width: 209px;
+  }
+`;
 
 export const ButtonStyled = styled.button<{
   backgroundcolor: string;
@@ -9,6 +18,7 @@ export const ButtonStyled = styled.button<{
   display: flex;
   height: 48px;
   padding: 16px 30px;
+  justify-content: space-between;
   align-items: center;
   gap: 8px;
   border-radius: 30px;
@@ -17,5 +27,7 @@ export const ButtonStyled = styled.button<{
   font-style: normal;
   font-weight: 700;
   line-height: 100%;
-  max-width: fit-content;
+  /* max-width: fit-content; */
+  /* width: 194px; */
+  animation: ${fadeIn} 2s infinite ease-out;
 `;
