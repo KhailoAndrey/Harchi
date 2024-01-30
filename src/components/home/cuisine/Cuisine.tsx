53ad6d832a9d9a7fd15a200f6886c90d8a6a1098
KhailoAndrey <hap512@gmail.com>
2024-01-30 19:23:34 +0200

    add two compopents to Home
    add LastRecipes
    add Cuisine

---
+++ src/components/home/cuisine/Cuisine.tsx
@@ -0,0 +1,25 @@
+import RedirectButton from '@/helpers/buttons/RedirectButton';
+import { Container, RecipesList, SubTitle, Title } from './Cuisine.styled';
+import { gradient, palette } from '@/constants/colors';
+
+function Cuisine() {
+  return (
+    <Container>
+      <Title>Українська кухня</Title>
+      <SubTitle>
+        Смак України в кожному страві: віддайте своїм смаковим почуттям
+        українській кухні!
+      </SubTitle>
+      <RecipesList />
+      <div className="redirbtn">
+        <RedirectButton
+          text={'Дивитись більше'}
+          backgroundcolor={gradient.red2}
+          textcolor={palette.mainWhite}
+        ></RedirectButton>
+      </div>
+    </Container>
+  );
+}
+
+export default Cuisine;
