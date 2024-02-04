72e3b47a5c873d07fe045ed0d010f6d71b70a1e0
KhailoAndrey <hap512@gmail.com>
2024-02-04 02:15:53 +0200

    refactor to ReactHookForm

--- src/components/modals/findRecipe/FindRecipe.styled.ts
+++ src/components/modals/findRecipe/FindRecipe.styled.ts
@@ -1,5 +1,8 @@
+import { React } from 'react';
 import { palette } from "@/constants/colors";
 import styled from "styled-components";
+import { Field, Form, Formik } from 'formik';
+
 
 export const Overlay = styled.div`
   position: fixed;
@@ -14,27 +17,103 @@ export const Overlay = styled.div`
   z-index: 10;
 `;
 
-export const ModalWindow = styled.div`
-  /* position: relative; */
-  top: 50%;
-  left: 50%;
-  width: 895px;
-  height: 535px;
-  padding: 33px 81px;
-  background-color: ${palette.mainWhite};
-  box-shadow: 3px 8px 14px 0px ${palette.lightGrey};
-  border-radius: 14px;
+export const ModalWrapper = styled.div`
+position: relative;
+/* top: 50%;
+left: 50%; */
+width: 895px;
+height: 535px;
+padding: 33px 81px;
+background-color: ${palette.mainWhite};
+box-shadow: 3px 8px 14px 0px ${palette.lightGrey};
+border-radius: 14px;  
+`
+export const CloseBtn = styled.button`
+position: absolute;
+top: 22px;
+right: 22px;
+  width: 24px;
+  height: 24px;
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+`
 
-  /* @media screen and (min-width: 320px) {
-    width: 280px;
-  }
-  @media screen and (min-width: 768px) {
-    top: 148px;
-    padding: 60px 129px;
-    width: 608px;
-    height: 354px;
-  }
-  @media screen and (min-width: 1280px) {
-    top: 112px;
-  } */
+export const ModalWindow = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+padding: 0;
+`;
+
+export const ModalTitle = styled.h2`
+  font-weight: 700;
+  font-size: 32px;
+  font-family: 'KyivType Sans';
+  color: ${palette.mainBlack};
+  line-height: 130%;
+  margin-bottom: 32px;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ModalForm = styled.div`
+width: 733px;
+display: flex;
+justify-content: space-between;
+margin-bottom: 24px;
+/* flex-direction: column; */
+/* margin-bottom: 48px;   */
+`
+export const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* gap: 24px; */
+  width: 336px;
+`
+
+export const Label = styled.label`
+  color: ${palette.mainBlack};
+  font-family: 'El Messiri';
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%;
+  margin-bottom: 16px;
+`;
+
+export const Input = styled.input`
+  display: flex;
+  width: 336px;
+  height: 48px;
+  padding: 16px;
+  justify-content: flex-end;
+  align-items: center;
+  /* gap: 16px; */
+  border-radius: 4px;
+  margin-bottom: 24px;
+  border: 1px solid ${palette.grey};
+  background: ${palette.mainWhite};
+  `;
+
+export const Select = styled.select`
+  display: flex;
+  width: 336px;
+  height: 48px;
+  padding: 16px;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 16px;
+  border-radius: 4px;
+  margin-bottom: 24px;
+  border: 1px solid ${palette.grey};
+  background: ${palette.mainWhite};  
 `;
--- src/components/modals/findRecipe/FindRecipe.tsx
+++ src/components/modals/findRecipe/FindRecipe.tsx
@@ -1,9 +1,63 @@
 import { useEffect } from 'react';
 import { useModal } from '../../../utils/context/Context';
-import { ModalWindow, Overlay } from './FindRecipe.styled';
+import {
+  Block,
+  CloseBtn,
+  FormContainer,
+  Input,
+  Label,
+  ModalForm,
+  ModalTitle,
+  ModalWindow,
+  ModalWrapper,
+  Overlay,
+  Select,
+} from './FindRecipe.styled';
+import { LuUtensilsCrossed } from 'react-icons/lu';
+import SubmitButton from '@/helpers/buttons/SubmitButton';
+import { gradient, palette } from '@/constants/colors';
+// import { Field, Form, Formik } from 'formik';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+enum TypeDishEnum {
+  breakfest = 'Сніданки',
+  firstmeal = 'Перші страви',
+  maincourses = 'Другі страви',
+  meatdishes = 'Страви з мʼяса',
+  fishdishes = 'Страви з риби',
+  salads = 'Салати',
+  snacks = 'Закуски',
+  dessert = 'Десерти',
+}
+
+enum CuisineEnum {
+  ukrainian = 'Українська',
+  georgian = 'Грузинська',
+  polish = 'Польська',
+  eastern = 'Східна',
+  italian = 'Італійська',
+  jewish = 'Єврейська',
+  french = 'Французька',
+  moroccan = 'Мароканська',
+  greek = 'Грецька',
+  japanese = 'Японська',
+  european = 'Європейська',
+  american = 'Американська',
+}
+
+interface IFormInput {
+  keywords: string;
+  typedish: TypeDishEnum;
+  cuisine: CuisineEnum;
+}
 
 function FindRecipe() {
   const { isModalOpen, closeModal } = useModal();
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data);
+    closeModal();
+  };
 
   const handleBackdropClick = event => {
     if (event.target === event.currentTarget) {
@@ -16,7 +70,6 @@ function FindRecipe() {
         closeModal();
       }
     };
-
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
@@ -26,7 +79,125 @@ function FindRecipe() {
   return (
     isModalOpen && (
       <Overlay onClick={handleBackdropClick}>
-        <ModalWindow>Підбір рецепту</ModalWindow>
+        <ModalWrapper>
+          <CloseBtn onClick={closeModal}>
+            <LuUtensilsCrossed />
+          </CloseBtn>
+          <ModalWindow>
+            <ModalTitle>Підбір рецепту</ModalTitle>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormContainer>
+                <ModalForm>
+                  <Block>
+                    <Label>Пошук по ключовим словам</Label>
+                    <Input {...register('keywords')} />
+                    <Label>Тип страви</Label>
+                    <Select {...register('typedish')}>
+                      <option value="breakfest">Сніданки</option>
+                      <option value="firstmeal">Перші страви</option>
+                      <option value="maincourses">Другі страви</option>
+                      <option value="meatdishes">Страви з мʼяса</option>
+                      <option value="fishdishes">Страви з риби</option>
+                      <option value="salads">Салати</option>
+                      <option value="snacks">Закуски</option>
+                      <option value="dessert">Десерти</option>
+                    </Select>
+                    <Label>Кухня</Label>
+                    <Select {...register('cuisine')}>
+                      <option value="ukrainian">Українська</option>
+                      <option value="georgian">Грузинська</option>
+                      <option value="polish">Польська</option>
+                      <option value="eastern">Східна</option>
+                      <option value="italian">Італійська</option>
+                      <option value="jewish">Єврейська</option>
+                      <option value="french">Французька</option>
+                      <option value="moroccan">Мароканська</option>
+                      <option value="greek">Грецька</option>
+                      <option value="japanese">Японська</option>
+                      <option value="european">Європейська</option>
+                      <option value="american">Американська</option>
+                    </Select>
+                  </Block>
+                  <Block>Right block</Block>
+                </ModalForm>
+                <SubmitButton
+                  text={'Підібрати рецепт'}
+                  backgroundcolor={gradient.buttonOrange}
+                  textcolor={palette.mainWhite}
+                />
+              </FormContainer>
+            </form>
+            {/* <Formik
+              initialValues={{
+                keywords: '',
+              }}
+              onSubmit={async values => {
+                await new Promise(r => setTimeout(r, 500));
+                alert(JSON.stringify(values));
+                closeModal();
+              }}
+            >
+              <Form>
+                <ModalForm>
+                  <Block>
+                    <label htmlFor="keywords">Пошук по ключовим словам</label>
+                    <Field
+                      id="keywords"
+                      name="keywords"
+                      placeholder="Введіть ключове слово"
+                    />
+                    <label htmlFor="typedish">Тип страви</label>
+
+                    <Field
+                      id="typedish"
+                      name="typedish"
+                      placeholder="Оберіть тип страви"
+                    />
+                    <label htmlFor="cuisine">Кухня</label>
+
+                    <Field
+                      id="cuisine"
+                      name="cuisine"
+                      placeholder="Оберіть кухню"
+                    />
+                  </Block>
+                  <Block>
+                    <label htmlFor="time">Час приготування</label>
+
+                    <Field id="time" name="time" placeholder="Бажаний час" />
+                    <label htmlFor="calories">Кількість калорій</label>
+
+                    <Field
+                      id="calories"
+                      name="calories"
+                      placeholder="Кількість калорій"
+                    />
+                    <label htmlFor="wishproducts">Має містити продукти</label>
+
+                    <Field
+                      id="wishproducts"
+                      name="wishproducts"
+                      placeholder="Введіть продукти: курятина, помідори..."
+                    />
+                    <label htmlFor="withoutproducts">Без продуктів</label>
+
+                    <Field
+                      id="withoutproducts"
+                      name="withoutproducts"
+                      placeholder="Введіть продукти: горіхи, мед..."
+                    />
+                  </Block>
+                </ModalForm>
+                <SubmitButton
+                  text={'Підібрати рецепт'}
+                  backgroundcolor={gradient.buttonOrange}
+                  textcolor={palette.mainWhite}
+                  // onClick={onSubmit}
+                />
+              </Form>
+            </Formik> */}
+          </ModalWindow>
+        </ModalWrapper>
       </Overlay>
     )
   );
