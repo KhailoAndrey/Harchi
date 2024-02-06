77d122a903814612434fad7b3e40ac0ac68a253c
KhailoAndrey <hap512@gmail.com>
2024-02-06 17:04:35 +0200

    Create LoadMoreIcon.tsx

---
+++ src/components/icons/LoadMoreIcon.tsx
@@ -0,0 +1,24 @@
+import { IoIosArrowDown } from 'react-icons/io';
+
+const LoadMore = () => {
+  return (
+    <>
+      <svg width="0" height="0">
+        <linearGradient
+          id="gradient"
+          x1="24.9675"
+          y1="21.3096"
+          x2="26.8833"
+          y2="32.2203"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.385" stopColor="#EA6500" />
+          <stop offset="0.965" stopColor="#FFCE20" />
+        </linearGradient>
+      </svg>
+      <IoIosArrowDown size={48} style={{ fill: 'url(#gradient)' }} />
+    </>
+  );
+};
+
+export default LoadMore;
